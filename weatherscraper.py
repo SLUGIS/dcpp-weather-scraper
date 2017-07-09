569e4587ba554ff0b4dad286f1c3eb7eb526fdee
trevorelsbree <trevorlelsbree@gmail.com>
2017-07-09 10:54:33 -0700

    finish stale data check

--- weatherscraper.py
+++ weatherscraper.py
@@ -49,26 +49,6 @@ def parse_wind_dir(dir):
     return str(int(val))
 
 
-# Reads the file to determine if the website data hasn't changed
-# since last run of script
-def is_new_data(file_path, row_str):
-
-    f = open(file_path, 'r')
-    for line in f:
-        pass
-    last = line
-
-    old_date = last[0:16]
-    new_date = row_str[0:16]
-
-    if old_date == new_date:
-        return False
-
-    print(old_date)
-    print(new_date)
-
-    return True
-
 # returns data to be written to .obs file
 # mm/dd/yyyy,hh:mm,[Temp F],[Dew Point F],[Wind Direction Degrees] 
 # [Wind speed MPH],[sky conditions (%)],[weather],[station pressure (mb)]
@@ -76,6 +56,7 @@ def parse_row(row):
   
     date_object = datetime.strptime(row[table_columns["last_update"]], "%I:%M%p, %A, %B %d, %Y")
     date_str = (datetime.strftime(date_object, "%m/%d/%Y,%H:%M"))
+    print(date_str)
   
     seq = (date_str, "-99", "-99", parse_wind_dir(row[table_columns["wind_dir"]]), 
             parse_wind_speed(row[table_columns["wind_speed_msec"]]),
@@ -122,7 +103,6 @@ def build_folder_name():
 def main():
     # where data files are written to
     save_path = 'C:/NRC/RASCAL43/Downloaded_Met_Data/'
-    #save_path = 'C:/Users/telsbree/git/pge-weather-scraper/'
     folder_name = build_folder_name()
         
     # joins the specified directory with the folder name    
@@ -133,40 +113,45 @@ def main():
         os.makedirs(folder_path)
 
     web_data = parse_website("https://www.pge.com/about/edusafety/dcpp/index.jsp")
-    #print(web_data)
 
     # for each tower, create or append to file
     # write a line of data to file
     for row in web_data:
         tower = met_towers[row[table_columns["tower_name"]]]
         file_path = os.path.join(folder_path, tower + '.obs')
-  
-      # check to see if file exists
-    if os.path.isfile(file_path):
-        new_file = False
-    else:
-        new_file = True
-  
-    f = open(file_path, 'a')
-    # write column header if file is
-    if new_file:
-        f.write("date (mm/dd/yyyy),time (hh:mm),temperature (F),dew point (F),")
-        f.write("wind direction (deg),wind speed (mph),sky conditions (%),weather,station pressure (mb), stability (A-G)\n")
-
-    # write formatted data to file
-    row_list = parse_row(row)
-      
-    if row_list[table_columns["wind_dir"]] == "invalid wind dir":
-        print ("invalid wind dir found")
-    elif row_list[table_columns["wind_speed_msec"]] == "invalid wind":
-        print ("invalid wind found")
-    else:
-        s = ","
-        row_str = s.join(row_list)
         
-    if is_new_data(file_path, row_str):
-        f.write(row_str + "\n")
-    f.close()
+        # check to see if file exists
+        # write column header if file is new
+        if not os.path.isfile(file_path):
+            f = open(file_path, 'w')
+            f.write("date (mm/dd/yyyy),time (hh:mm),temperature (F),dew point (F),")
+            f.write("wind direction (deg),wind speed (mph),sky conditions (%),weather,station pressure (mb), stability (A-G)\n")
+            f.close()
+        
+        # write formatted data to file
+        row_list = parse_row(row)
+      
+        if row_list[table_columns["wind_dir"]] == "invalid wind dir":
+            print ("invalid wind dir found")
+        elif row_list[table_columns["wind_speed_msec"]] == "invalid wind":
+            print ("invalid wind found")
+        else:
+            s = ","
+            row_str = s.join(row_list)
+       
+        f = open(file_path, 'r')
+        for line in f:
+            pass
+        last = line
+        f.close()
+
+        old_timestamp = last[0:16]
+        new_timestamp = row_str[0:16]
+
+        if old_timestamp != new_timestamp:
+            f = open(file_path, 'a')
+            f.write(row_str + "\n")
+            f.close()
 
 
 if __name__ == "__main__":
